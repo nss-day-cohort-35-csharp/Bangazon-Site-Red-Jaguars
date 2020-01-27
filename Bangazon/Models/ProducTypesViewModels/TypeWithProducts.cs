43f332624b1f69e46dcb2ca93fbe4f70dfb358ef
unknown <fortunato.mugnano88@gmail.com>
2020-01-27 15:45:30 -0600

    ticket 11 done

---
+++ Bangazon/Models/ProducTypesViewModels/TypeWithProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProducTypesViewModels
+{
+    public class TypeWithProducts
+    {
+        public int TypeId { get; set; }
+        [Display(Name = "Categories")]
+        public string TypeName { get; set; }
+
+        
+        public int ProductCount { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
